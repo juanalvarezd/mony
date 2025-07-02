2b0bbe19e594c84d44982439b50811b1c5ef0069
j-c-a-d <j.c.a.d@hotmail.com>
2025-07-02 02:17:28 -0300

    maquetado de la seccion signup

--- src/features/auth/components/AuthForm/SignupForm.tsx
+++ src/features/auth/components/AuthForm/SignupForm.tsx
@@ -1,8 +1,15 @@
-
+import FormGroup from "@/shared/components/FormGroup/FormGroup"
+import { Input } from "@/shared/components/Input/Input"
+import styles from "@/features/auth/components/AuthForm/form.module.css"
+import { InputPassword } from "@/shared/components/Input/InputPassword"
+import Button from "@/shared/components/Button/Button"
 const SignupForm = () => {
   return (
-    <form>
-      signup form
+    <form className={styles["form"]}>
+      <FormGroup as={Input} type="text" id="name" label="Nombre" placeholder="Ingrese su nombre" />
+      <FormGroup as={Input} type="email" id="email" label="Email" placeholder="Ingrese su correo" />
+      <FormGroup as={InputPassword} type="password" id="password" label="Cree una contraseña" placeholder="Contraseña" />
+      <Button type="submit" variant="dark" size="btnW100">Registrate</Button>      
     </form>
   )
 }
--- src/features/auth/pages/SignupPage/SignupPage.tsx
+++ src/features/auth/pages/SignupPage/SignupPage.tsx
@@ -1,9 +1,9 @@
 import SignupForm from "../../components/AuthForm/SignupForm"
-
+import styles from "@/features/auth/pages/SignupPage/signupPage.module.css"
 const SignupPage = () => {
   return (
-    <main>
-      <h3>Sign Up</h3>
+    <main className={styles["main"]}>
+      <h1>Registrate</h1>
       <SignupForm />
     </main>
   )
