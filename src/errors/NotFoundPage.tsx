f54597bda64c2bca86e7a637e359ee50f5bbc360
Juan Alvarez <juanalvarezd87@gmail.com>
2025-07-09 04:08:32 -0300

    feat:Cambios en responsive de formulario y correccion en redireccionamiento cuando la ruta es incorrecta

--- src/errors/NotFoundPage.tsx
+++ src/errors/NotFoundPage.tsx
@@ -1,9 +1,9 @@
 import styles from "@/errors/errors.module.css"
 import { useEffect, useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const NotFoundPage = () => {
-  const [countdown, setCountdown] = useState(1000)
+  const [countdown, setCountdown] = useState(8)
   const navigate = useNavigate();
   useEffect(() => {
     const counter = setInterval(() => {
@@ -24,8 +24,8 @@ const NotFoundPage = () => {
   return (
     <div className={styles["errorPage"]}>
       <div className={styles["notFoundImage"]}></div>
-      <Link to={"/"} className={styles["link"]}>Ir a inicio</Link>
-      <p>Redireccionando en <span className={styles["countdown"]}>{countdown}</span> </p>
+      {history.length > 2 && <p>Página no encontrada, <button className={styles["return"]} onClick={() => navigate(-1)}>volver atrás</button></p>}
+      <p>Redireccionando a Inicio en <span className={styles["countdown"]}>{countdown}</span> </p>
     </div>
   )
 }
